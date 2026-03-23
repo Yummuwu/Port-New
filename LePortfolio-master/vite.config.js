62f56778d88ea7f3ce10d5bb18d2c9e6f2f94ed9
Yummuwu <a.cappello38@gmail.com>
2026-03-23 09:44:34 +0100

    23/03 second

--- LePortfolio-master/vite.config.js
+++ LePortfolio-master/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 export default defineConfig({
-  base: '/Yummuwu.github.io/', // ✅ AJOUT ICI
+  base: '/Port-New/', // ✅ AJOUT ICI
 
   plugins: [react(), tailwindcss()],
   
